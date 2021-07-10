de6b536a5e164c6d020d833f1468dd0853edae4a
Wei Lee <weilee.rx@gmail.com>
2021-07-10 23:13:04 +0800

    feat(cli/plot): add plot premium rate command

--- bahamut_ani_stat/__main__.py
+++ bahamut_ani_stat/__main__.py
@@ -2,6 +2,7 @@ import click
 
 from bahamut_ani_stat.cli.db_commands import db_command_group
 from bahamut_ani_stat.cli.parse_commands import parse_command_group
+from bahamut_ani_stat.cli.plot_commands import plot_command_group
 
 
 @click.group()
@@ -11,6 +12,7 @@ def main():
 
 main.add_command(parse_command_group)
 main.add_command(db_command_group)
+main.add_command(plot_command_group)
 
 
 if __name__ == "__main__":
