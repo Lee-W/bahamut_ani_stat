95d17fddb408895167668890ca2f683ebc3f469a
Wei Lee <weilee.rx@gmail.com>
2021-07-25 00:50:47 +0800

    refactor(plot): refactor query sql

--- bahamut_ani_stat/plot/static/anime-filter.js
+++ bahamut_ani_stat/plot/static/anime-filter.js
@@ -1,17 +1,17 @@
 var indices = [];
 for (var i = 0; i < data_source.get_length(); i++) {
-    let anime_score = data_source.data["anime_scores"][i];
-    let anime_view_count = data_source.data["anime_view_counts"][i];
+    let score = data_source.data["score"][i];
+    let view_count = data_source.data["view_count"][i];
     let anime_name_lower = data_source.data["name"][i].toLowerCase();
 
     if (
-        anime_score >= score_slider.value[0] &&
-        anime_score <= score_slider.value[1] &&
-        anime_view_count >= view_counter_silider.value[0] &&
-        anime_view_count <= view_counter_silider.value[1] &&
+        score >= score_slider.value[0] &&
+        score <= score_slider.value[1] &&
+        view_count >= view_counter_silider.value[0] &&
+        view_count <= view_counter_silider.value[1] &&
         !(only_new_toggle.active && !data_source.data["is_new"][i]) &&
-        !(ignore_wip_toggle.active && anime_score == -1) &&
-        !(ignore_wip_toggle.active && anime_view_count == -1) &&
+        !(ignore_wip_toggle.active && score == -1) &&
+        !(ignore_wip_toggle.active && view_count == -1) &&
         !(text_input.value.length > 0 && !anime_name_lower.includes(text_input.value.toLowerCase()))
     ) {
         indices.push(true);
