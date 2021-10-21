2ec57707039b3999bd4be019dc515da0d661d1bb
Wei Lee <weilee.rx@gmail.com>
2021-10-21 18:56:41 +0800

    style: reformat alembic

--- tasks/common.py
+++ tasks/common.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
 VENV_PREFIX = "uv run"
-_COMMON_TARGETS = ["bahamut_ani_stat", "tests", "tasks"]
+_COMMON_TARGETS = ["bahamut_ani_stat", "tests", "tasks", "alembic"]
 COMMON_TARGETS_AS_STR = " ".join(_COMMON_TARGETS)
