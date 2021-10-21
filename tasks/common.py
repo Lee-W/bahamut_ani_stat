bdd3321739888c67340393e224ec9d8df06e95b4
Wei Lee <weilee.rx@gmail.com>
2021-10-21 18:56:41 +0800

    style: reformat alembic

--- tasks/common.py
+++ tasks/common.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
 VENV_PREFIX = "poetry run"
-_COMMON_TARGETS = ["bahamut_ani_stat", "tests"]
+_COMMON_TARGETS = ["bahamut_ani_stat", "tests", "alembic"]
 COMMON_TARGETS_AS_STR = " ".join(_COMMON_TARGETS)
