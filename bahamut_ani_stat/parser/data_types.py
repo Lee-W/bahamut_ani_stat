a656c85326d497091b3def3d3614f1bbb22269a5
Wei Lee <weilee.rx@gmail.com>
2024-08-13 23:36:39 +0800

    test: fix test break due to pydantic upgrade

--- bahamut_ani_stat/parser/data_types.py
+++ bahamut_ani_stat/parser/data_types.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from datetime import datetime  # noqa: TCH003
+from typing import Optional, Union
 
 from pydantic import BaseModel
-
-if TYPE_CHECKING:
-    from datetime import datetime
 
 
 class Danmu(BaseModel):
@@ -19,35 +17,39 @@ class Danmu(BaseModel):
 
 
 class Episode(BaseModel):
-    sn: str | None = None
-    name: str | None = None
-    season_title: str | None = None
-    upload_date: datetime | str | None = (
+    sn: Optional[str] = None
+    name: Optional[str] = None
+    season_title: Optional[str] = None
+    upload_date: Union[datetime, Optional[str]] = (
         None  # TODO: Fix tests and type conflict (str and datetime)
     )
-    view_count: int | None = None
+    view_count: Optional[int] = None
 
 
 class AnimeScore(BaseModel):
     score: float
     reviewer_count: int
 
-    five_star_percentage: float | None = None
-    four_star_percentage: float | None = None
-    three_star_percentage: float | None = None
-    two_star_percentage: float | None = None
-    one_star_percentage: float | None = None
+    five_star_percentage: Optional[float] = None
+    four_star_percentage: Optional[float] = None
+    three_star_percentage: Optional[float] = None
+    two_star_percentage: Optional[float] = None
+    one_star_percentage: Optional[float] = None
 
 
 class Anime(BaseModel):
     sn: str
-    name: str | None = None
-    view_count: int | None = None
-    release_time: datetime | None = None
-    metadata: dict[str, str] | None = None
-    upload_hour: str | None = None
-    labels: list[str] | None = None
+    name: Optional[str] = None
+    view_count: Optional[int] = None
+    release_time: Optional[datetime] = None
+    metadata: Optional[dict[str, str]]= None
+    upload_hour: Optional[str] = None
+    labels: Optional[list[str]] = None
 
-    anime_score: AnimeScore | None = None
-    episodes: list[Episode] | None = None
-    dammus: list[Danmu] | None = None
+    anime_score: Optional[AnimeScore] = None
+    episodes: Optional[list[Episode]] = None
+    dammus: Optional[list[Danmu]] = None
+
+
+Anime.model_rebuild()
+Episode.model_rebuild()
